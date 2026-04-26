606d02773552aaba3caa9c151bf66be52a5ffa1b
Mauro Peixoto <mauroaugustodev@gmail.com>
2026-04-26 19:46:43 -0300

    definicoes de variaveis

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	pizza := "Pepperoni"
+	pizzariaInstram, telefone := "Pizzaria do Zé", "1234-5678"
+
+	fmt.Printf("Eu gosto de %s\n", pizza)
+	fmt.Printf("A pizzaria se chama %s e o telefone é %s\n", pizzariaInstram, telefone)
+	fmt.Println("Hello, World!")
+}
