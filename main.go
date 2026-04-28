4af24a07a5b9c3486b43f45c3b041ca003ab754e
Mauro Peixoto <mauroaugustodev@gmail.com>
2026-04-28 16:18:46 -0300

    Crud feito com o json

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
 	"pizzaria/models"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
-var pizzas = []models.Pizza{
-	{ID: 1, Preco: 29.99, Sabor: "Calabresa"},
-	{ID: 2, Preco: 34.99, Sabor: "Mussarela"},
-	{ID: 3, Preco: 39.99, Sabor: "Frango com Catupiry"},
-}
+var pizzas = []models.Pizza{}
 
 func main() {
+	loadPizzas()
 
 	router := gin.Default()
 
@@ -35,8 +35,9 @@ func createPizza(c *gin.Context) {
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
-
+	pizza.ID = len(pizzas) + 1
 	pizzas = append(pizzas, pizza)
+	savePizzas()
 
 	c.JSON(201, pizza)
 }
@@ -58,4 +59,35 @@ func getPizzaByID(c *gin.Context) {
 	}
 
 	c.JSON(404, gin.H{"error": "Pizza não encontrada"})
+}
+
+func loadPizzas() {
+
+	file, err := os.Open("./dados/pizza.json")
+	if err != nil {
+		fmt.Print("Erro ao abrir o arquivo:", err)
+		return
+	}
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&pizzas); err != nil {
+		fmt.Print("Erro ao decodificar o arquivo:", err)
+		return
+	}
+	defer file.Close()
+
+}
+
+func savePizzas() {
+	file, err := os.Create("./dados/pizza.json")
+	if err != nil {
+		fmt.Print("Erro ao criar o arquivo:", err)
+		return
+	}
+
+	encoder := json.NewEncoder(file)
+	if err := encoder.Encode(pizzas); err != nil {
+		fmt.Print("Erro ao codificar o arquivo:", err)
+		return
+	}
+
 }
