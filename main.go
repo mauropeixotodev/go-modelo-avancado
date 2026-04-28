3173240386d1f8f80c9a01bbd6d014a7eae58bb8
Mauro Peixoto <mauroaugustodev@gmail.com>
2026-04-27 21:47:23 -0300

    Incluindo meotodo post

--- main.go
+++ main.go
@@ -6,14 +6,29 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var pizzas = []models.Pizza{models.Pizza{ID: 1, Preco: 29.99, Sabor: "Calabresa"}, models.Pizza{ID: 2, Preco: 34.99, Sabor: "Mussarela"}, models.Pizza{ID: 3, Preco: 39.99, Sabor: "Frango com Catupiry"}}
+
 func main() {
 
 	router := gin.Default()
 	router.GET("/pizzas", getPizzas)
+	router.POST("/pizzas", createPizza)
 	router.Run()
 }
 
 func getPizzas(c *gin.Context) {
-	var pizzas = []models.Pizza{models.Pizza{ID: 1, Preco: 29.99, Sabor: "Calabresa"}, models.Pizza{ID: 2, Preco: 34.99, Sabor: "Mussarela"}, models.Pizza{ID: 3, Preco: 39.99, Sabor: "Frango com Catupiry"}}
+
 	c.JSON(200, pizzas)
+}
+
+func createPizza(c *gin.Context) {
+	var pizza models.Pizza
+	if err := c.ShouldBindJSON(&pizza); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	pizzas = append(pizzas, pizza)
+	c.JSON(201, gin.H{"message": "Pizza criada com sucesso!"})
+
 }
